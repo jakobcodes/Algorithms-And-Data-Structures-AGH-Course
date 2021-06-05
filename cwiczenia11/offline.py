4b837146ae392c5868c192967a506ab60cba9338
unknown <jakub.lubkowski1@gmail.com>
2021-06-05 12:58:23 +0200

    create class and function

---
+++ cwiczenia11/offline.py
@@ -0,0 +1,20 @@
+from typing import Mapping
+
+
+class BSTNode:
+    def __init__(self, key) -> None:
+        self.key = key
+        self.left = None
+        self.right = None
+        self.parent = None
+    
+
+def insert(root, key):
+    pass
+
+def remove(root, key):
+    pass
+
+if __name__ == '__main__':
+    pass
+    
